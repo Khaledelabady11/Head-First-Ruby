cbf8d21c9efbfff37f5e05e6966cf9c7a49f231d
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2022-07-13 22:12:15 +0200

    User input

---
+++ chapter1.rb
@@ -0,0 +1,29 @@
+#Head First Ruby
+#Chapter 1 /  More With Less
+
+
+#Hello World
+puts "Hello World"
+
+#///////////////////
+
+# puts 2+3                //5
+
+#puts 2+2==5              // false
+
+#name = "Zaphod"
+#name.upcase              # ZAPHOD
+#name.reverse             #“dohpaZ"
+#name.class               #String
+#name * 3                 #“ZaphodZaphodZaphod"
+puts "second line", "third line", "fourth line"
+print "second line", "third line", "fourth line"
+#/////////////////////////////////////
+
+
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+
+input = gets
+puts "Welcome #{input}"
+
