7a36d8d2fc7d64ba3a86ccea3e9b1042d52bfc9b
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2022-07-14 07:52:00 +0200

    more with less

--- chapter1.rb
+++ chapter1.rb
@@ -16,14 +16,27 @@ puts "Hello World"
 #name.reverse             #“dohpaZ"
 #name.class               #String
 #name * 3                 #“ZaphodZaphodZaphod"
-puts "second line", "third line", "fourth line"
-print "second line", "third line", "fourth line"
-#/////////////////////////////////////
+#puts "second line", "third line", "fourth line"
+#print "second line", "third line", "fourth line"
 
 
 puts "Welcome to 'Get My Number!'"
 print "What's your name? "
 
-input = gets
-puts "Welcome #{input}"
+input = gets             #reads a line from standard input
+
+
+name =input.chomp        #We can use the chomp method to remove the newline character.
+
+
+puts "Welcome #{name} !"       #the exclamation point jumps down to the next line!
+
+# puts 42.methods
+
+ #The rand method will generate a random number within a given range.
+puts rand(100)          #(0 : 99)
+puts rand(100) +1         #(1 : 100)
+
+puts 24.to_f
+
 
